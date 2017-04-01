33ba86534e811b861a1587abae648d5fea3462e2
taoqun <taoqun@317hu.com>
2017-04-01 13:40:17 +0800

    1、翻页功能，未完成

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -4,37 +4,36 @@ class Tools{
     }
 
     ngSelPage(res) {
-          if(0 === res.data.code) {
-            this.arrayList = res.data.data
-            this.pageInfo = res.data.page
-            this.pageInfo.totalPage = 11
-            this.pageInfo.pageNo = 7
+        console.log(res);
+            this.arrayList = res.data.data.result
+            this.lastPageNumber = res.data.data.lastPageNumber
+            this.currentPage = res.data.data.pageNumber
+            console.log(this.lastPageNumber);
             let i = 1;
-            if(this.pageInfo.totalPage<10) {
-              for(i = 1 ;i<=this.pageInfo.totalPage;i++) {
+            if(this.lastPageNumber<10) {
+              for(i = 1 ;i<=this.lastPageNumber;i++) {
                 this.pagination[i-1] = i;
               }
             }else{
-              let cur = this.pageInfo.pageNo
+              let cur = this.currentPage
               if(cur<=4){
                 for( i = 1 ;i<=10;i++) {
                   this.pagination[i-1] = i;
                 }
               }else{
-                let lengthToEnd = this.pageInfo.totalPage - this.pageInfo.pageNo
+                let lengthToEnd = this.lastPageNumber - this.currentPage
                 if(lengthToEnd>6){
                   let j = 0
-                  for(i = this.pageInfo.pageNo -4, j = 0;i<(this.pageInfo.pageNo+6);i++,j++){
+                  for(i = this.currentPage -4, j = 0;i<(this.currentPage+6);i++,j++){
                     this.pagination[j] = i
                   }
                 }else{
                   let k = 0
-                  for(i = this.pageInfo.totalPage - 9, k = 0;i<=this.pageInfo.totalPage;i++,k++) {
+                  for(i = this.lastPageNumber - 9, k = 0;i<=this.lastPageNumber;i++,k++) {
                       this.pagination[k] = i
                   }
                 }
               }
-            }
           }
           return this.pagination
     }
